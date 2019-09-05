be0741c94722ddfeaf0dad39b58934fe7516534b
wsx9412 <wsx9412@naver.com>
2019-09-05 17:06:27 +0900

    another syntax

---
+++ Node.js/syntax/template.js
@@ -0,0 +1,8 @@
+var name = 'k880';
+var letter = 'Lorem ipsum dolor'+name+' sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim'+name+' veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in r'+name+'eprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,'+name+' sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+var lettr2 = `Lorem ipsum dolor ${name} sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ${name} veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in r ${name} eprehenderit in volupt
+
+ate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, ${name} sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+
+console.log(lettr2);
